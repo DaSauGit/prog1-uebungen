0205a819fec09d71a89f679cd85cec5238ff4315
Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>
2025-01-27 15:40:36 +0100

    Klausurvorbereitung Signed-off-by: Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>
    Signed-off-by: Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>

--- aufgabe6/UnbenoteteLeistung.java
+++ aufgabe6/UnbenoteteLeistung.java
@@ -25,12 +25,10 @@ public class UnbenoteteLeistung extends aufgabe6.Leistung {
         super(f);
         this.bestanden = b;
     }
-
     @Override
     public boolean istBestanden() {
         return bestanden;
     }
-
     @Override
     public boolean istBenotet() {
         return false;
